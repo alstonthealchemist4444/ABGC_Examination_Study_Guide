3c47577dde82145a6c499528288682626587e606
Alston Edwards <laedwards3@uncg.edu>
2026-07-14 03:57:40 -0400

    Fix typo in conditionName and notes fields

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const conditions = [
   {
-    conditionName: "22q11.2 del syndrome (DiGeorge or Velocardiofacial Syndrome),
+    conditionName: "22q11.2 del syndrome (DiGeorge or Velocardiofacial Syndrome)",
     diseaseCategory: "Microdel/dup",
     gene: "TBX1",
     inheritance: "AD",
@@ -13,7 +13,7 @@ const conditions = [
     otherWorkUp: "EKG, spine radiographs, renal ultrasound, CBC with differential, parathyroid hormone, Eval by: opthalmology, audiology, or immunology, ENT.",
     management: "Feeding therapy, developmental services, surgical repair of cardiac/palatal changes, PRN Ca+2 supplement.",
     clinicalPearl: "CATCH22: Cardiac changes, Abnormal facies, Thymic hypoplasia, Cleft palate, Hypocalcemia, 22q.11del",
-    notes: "TBX1 is the deleted gene responsible for cardiac symptoms."
+    notes: "TBX1 is the deleted gene responsible for cardiac symptoms.",
     terminologyToKnow: " Codominance: two different alleles are both fully expressed in an individual's phenotype i.e. Alpha-1 Antitrypsin Deficiency."
   },
 
