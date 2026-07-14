57847096cbcbac1c90dfef6743c957a8f28e2f8e
Alston Edwards <laedwards3@uncg.edu>
2026-07-14 03:25:17 -0400

    Add conditions to script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+const conditions = [
+  {
+    conditionName: "22q11.2 del syndrome (DiGeorge or Velocardiofacial Syndrome),
+    diseaseCategory: "Microdel/dup",
+    gene: "TBX1",
+    inheritance: "AD",
+    majorClinicalFeatures: "Palatal defects, cardiac defects, hypocalcemia, immunodeficiency due to hypoplasia, psychiatric illness i.e. ASD/Schizophrenia.", 
+    otherClinicalFeatures: "Short stature, hypertelorism, low set ears, micrognathia, learning differences, hearing/vision issues, renal changes, siezures.",
+    mechanismPathophysiology: "2.54 Mb del; de novo 90%",
+    diagnosis: "CMA! <-bold this later!!!!, Karotype, FISH",
+    majorWorkUp: " Echo, Ca+2 serum, thyroid function test.",
+    otherWorkUp: "EKG, spine radiographs, renal ultrasound, CBC with differential, parathyroid hormone, Eval by: ophthamology, audiology, or immunology, ENT.",
+    management: "Feeding therapy, developmental services, surgical repair of cardiac/palatal changes, PRN Ca+2 supplement.",
+    clinicalPearl: "CATCH22: Cardiac changes, Abnormal facies, Thymic hypoplasia, Cleft palate, Hypocalemia, 22q.11del",
+    notes: "TBX1 is the deleted gene responsible for cardiac symptoms."
+    terminologyToKnow: " Codominance: two different allels are both fully expressed in an individual's phenotype i.e. Alpha-1 Antitrypsin Deficiency."
+  }
+]
